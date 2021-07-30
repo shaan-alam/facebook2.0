5620cd6e77aeef02854e6c285368bded6f69a10c
Shaan Alam <shaanalam369@gmail.com>
2021-07-31 04:17:27 +0530

    docs: update user sign up controller documentations

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,14 +3,39 @@ import dotenv from "dotenv";
 import logger from "./logger";
 import dbConnect from "./db/connect";
 import routes from "./routes";
+import cors from "cors";
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Facebook 2.0 API",
+      version: "1.0.0",
+      description: "API Docs for Facebook 2.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.route.ts"],
+};
+
+const specs = swaggerJsDoc(options);
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 // Routing configuration
 routes(app);
--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -6,7 +6,46 @@ import { createUserSchema } from "../schema/auth.schema";
 
 const router = express.Router();
 
-// User sign up  route
+/**
+ * @swagger
+ * tags:
+ *  name: Authentication
+ *  description: The Authentication managing API
+ */
+
+/**
+ * @swagger
+ * /auth/signup:
+ *  post:
+ *    summary: Creates a new User
+ *    tags: [Authentication]
+ *    requestBody:
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              fullName:
+ *                type: string
+ *                description: Full name of the user
+ *              email:
+ *                type: string
+ *                description: Email of the user
+ *              password:
+ *                type: string
+ *                description: Password of the user
+ *            example:
+ *                fullName: Shaan Alam
+ *                email: shaanalam@test.com
+ *                password: ShaanIsCool_3000
+ *    responses:
+ *      200:
+ *        description: OK
+ *      400:
+ *        description: Bad Request
+ *
+ *
+ */
 router.post("/signup/", validateRequest(createUserSchema), getUser, signUp);
 
 export default router;
