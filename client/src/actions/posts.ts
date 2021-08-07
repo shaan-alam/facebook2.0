f918f798044adb43adc2e09fd489e69ff448d489
Shaan Alam <shaanalam369@gmail.com>
2021-08-07 17:15:07 +0530

    refactor: removed post related actions and used react-query instead

--- client/src/actions/posts.ts
+++ client/src/actions/posts.ts
@@ -18,46 +18,6 @@ import {
 import { POST_CONTAINER } from "../constants";
 
 /**
- * @description Action creator to fetch posts and dispatch a GET_POSTS action
- * @returns {[Promise<void>]} Promise<void>
- */
-export const getPosts: GetPostType = () => async (dispatch) => {
-  try {
-    const result: AxiosResponse<Post[]> = await api.getPosts();
-
-    dispatch({ type: GET_POSTS, payload: result.data });
-  } catch (err: any) {
-    dispatch({
-      type: ERROR,
-      payload: { ON: POST_CONTAINER, message: err.response.data.messagge },
-    });
-  }
-};
-
-/**
- * @description Action creator to send a new post to create to backend and dispatch a CREATE_POST action
- * @param {[Post]} post An object containing post details
- * @param {[String]} currentUserId The ID of the user who created the post.
- * @returns {[Prmoise<void>]} Promise<void>
- */
-export const createPost: CreatePostType =
-  (post, currentUserId) => async (dispatch) => {
-    try {
-      const result: AxiosResponse<Post> = await api.createPost({
-        ...post,
-        creator: currentUserId,
-      });
-
-      dispatch({ type: CREATE_POST, payload: result.data });
-    } catch (err) {
-      dispatch({
-        type: ERROR,
-        payload: { ON: POST_CONTAINER, message: err.response.data },
-      });
-    }
-  };
-
-/**
  * @description Action creator to make a DELETE request along with the ID and dispatch a DELETE_POSTS action
  * @param {[String]} id The ID of the post which is to be deleted.
  * @returns {[Promise<void>]} Promise<void>
@@ -84,23 +44,6 @@ export const deletePost: DeletePostType = (id) => async (dispatch) => {
 export const editPost: EditPostType = (id, newPost) => async (dispatch) => {
   try {
     const result: AxiosResponse<Post> = await api.editPost(id, newPost);
-    dispatch({ type: UPDATE_POST, payload: result.data });
-  } catch (err) {
-    dispatch({
-      type: ERROR,
-      payload: { ON: POST_CONTAINER, message: err.response.data.message },
-    });
-  }
-};
-
-/**
- * @description An action creator to make a PATCH request to like a post
- * @param {[String]} id The ID of the post which is to be liked
- * @returns {[Promise<void>]} Promise<void>
- */
-export const likePost: LikePostType = (id) => async (dispatch) => {
-  try {
-    const result: AxiosResponse<Post> = await api.likePost(id);
     dispatch({ type: UPDATE_POST, payload: result.data });
   } catch (err) {
     dispatch({
