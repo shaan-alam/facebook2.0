466eaf41b29e2d329a6e38f25fc28c2c94fe4599
Shaan Alam <shaanalam369@gmail.com>
2021-08-07 12:38:17 +0530

    fix: create separate component forlike and comment actions

---
+++ client/src/components/Posts/Post/PostActions/index.tsx
@@ -0,0 +1,78 @@
+import { ChatAltIcon, ThumbUpIcon } from "@heroicons/react/outline";
+import {
+  ChatAltIcon as ChatAltIconSolid,
+  ThumbUpIcon as ThumbUpIconSolid,
+} from "@heroicons/react/solid";
+
+const PostActions = ({
+  commentBox,
+  likes,
+  profile,
+}: {
+  commentBox: React.RefObject<HTMLInputElement>;
+  likes: Array<{ _id: string; fullName: string }>;
+  profile: any;
+}) => {
+  const didCurrentUserLike = likes.filter(
+    ({ _id }: { _id: string }) => _id === profile.user._id
+  );
+
+  const LikeComponent = () => {
+    if (likes.length === 1) {
+      console.log(1);
+      return (
+        <ThumbUpIconSolid className="w-14 mb-2 text-red-600 cursor-pointer px-4 py-2 rounded-lg" />
+      );
+    } else if (didCurrentUserLike !== null && likes.length > 2) {
+      console.log(2);
+      return (
+        <ThumbUpIconSolid className="w-14 mb-2 text-red-600 cursor-pointer px-4 py-2 rounded-lg" />
+      );
+    } else if (didCurrentUserLike !== null && likes.length > 2) {
+      console.log(3);
+      return (
+        <ThumbUpIconSolid className="w-14 mb-2 text-red-600 cursor-pointer px-4 py-2 rounded-lg" />
+      );
+    } else {
+      return (
+        <ThumbUpIcon className="w-14 mb-2 text-gray-600  cursor-pointer px-4 py-2 hover:bg-red-100 rounded-lg" />
+      );
+    }
+  };
+
+  const computeLikeText = () => {
+    let content = "";
+    if (likes.length === 1) {
+      console.log(1);
+      content = "1 Like";
+    } else if (didCurrentUserLike !== null && likes.length > 2) {
+      console.log(2);
+      content = `You and ${likes.length - 1} others like this`;
+    } else {
+      content = `${likes.length} likes`;
+    }
+
+    return content;
+  };
+
+  return (
+    <>
+      <div className="post-actions flex">
+        <LikeComponent />
+        <ChatAltIcon
+          className="w-14 mb-2 text-gray-600  cursor-pointer px-4 py-2 rounded-lg"
+          onClick={() => commentBox?.current?.focus()}
+        />
+      </div>
+      <div
+        className={`post-actions-description-text px-4 ${
+          didCurrentUserLike.length !== 0 ? "text-red-600 font-semibold" : ""
+        }`}
+      >
+        {computeLikeText()}
+      </div>
+    </>
+  );
+};
+
+export default PostActions;
--- client/src/components/Posts/Post/index.tsx
+++ client/src/components/Posts/Post/index.tsx
@@ -9,6 +9,7 @@ import {
   ChatAltIcon as ChatAltIconSolid,
   ThumbUpIcon as ThumbUpIconSolid,
 } from "@heroicons/react/solid";
+import PostActions from "./PostActions";
 
 const Post = ({ post }: { post: PostType }) => {
   const [isLoaded, setLoaded] = useState<boolean>(false);
@@ -16,60 +17,6 @@ const Post = ({ post }: { post: PostType }) => {
   const profile = JSON.parse(localStorage.getItem("profile") || "{}");
 
   const commentBox = useRef<HTMLInputElement>(null);
-
-  const LikeComponent = () => {
-    let content = "";
-    const didCurrentUserLike = post.likes.likes.filter(
-      ({ fullName }: { fullName: string }) => fullName === profile.user.fullName
-    );
-
-    console.log(didCurrentUserLike !== null);
-    if (post.likes.likes.length === 1) {
-      console.log(1);
-      return (
-        <span className="flex flex-col items-center text-red-600 font-semibold mr-6">
-          <ThumbUpIconSolid className="w-14 text-red-300 cursor-pointer px-4 py-2 hover:bg-red-300 rounded-lg" />
-          1 Like
-        </span>
-      );
-    } else if (didCurrentUserLike !== null && post.likes.likes.length > 2) {
-      console.log(2);
-      return (
-        <span className="flex flex-col  text-red-600 font-semibold mr-6">
-          <ThumbUpIconSolid className="w-14 mb-2 text-white cursor-pointer px-4 py-2 bg-red-600 rounded-lg" />
-          You and {post.likes.likes.length - 1} others like this
-        </span>
-      );
-    } else if (didCurrentUserLike !== null && post.likes.likes.length > 2) {
-      console.log(3);
-      return (
-        <span className="flex flex-col items-center text-red-600 font-semibold mr-6">
-          <ThumbUpIconSolid className="w-14 mb-2 text-white cursor-pointer px-4 py-2 bg-red-600 rounded-lg" />
-          {post.likes.likes.length} like this
-        </span>
-      );
-    } else {
-      return (
-        <span className="flex flex-col text-gray-600 font-semibold mr-6">
-          <ThumbUpIcon className="w-14 mb-2 text-gray-600  cursor-pointer px-4 py-2 hover:bg-red-100 rounded-lg" />
-          &nbsp; {post.likes.likes.length} Likes
-        </span>
-      );
-    }
-  };
-
-  const CommentComponent = () => {
-    return (
-      <>
-        <span className="flex items-start font-semibold mr-6">
-          <ChatAltIcon
-            className="w-14 mb-2 text-gray-600  cursor-pointer px-4 py-2 hover:bg-red-100 rounded-lg"
-            onClick={() => commentBox?.current?.focus()}
-          />
-        </span>
-      </>
-    );
-  };
 
   return (
     <div className="post my-8">
@@ -87,9 +34,12 @@ const Post = ({ post }: { post: PostType }) => {
           alt={post?.caption}
           className="my-3 w-full rounded-lg"
         />
-        <div className="post-actions flex">
-          <LikeComponent />
-          <CommentComponent />
+        <div className="post-actions">
+          <PostActions
+            commentBox={commentBox}
+            likes={post.likes.likes}
+            profile={profile}
+          />
         </div>
         <div className="add-comment ">
           <input
@@ -105,63 +55,3 @@ const Post = ({ post }: { post: PostType }) => {
 };
 
 export default Post;
-
-{
-  /* 
-<Card>
-        {!isLoaded && (
-          <Placeholder>
-            <Placeholder.Image square />
-          </Placeholder>
-        )}
-        <Image
-          style={isLoaded ? {} : { display: "none" }}
-          src={post?.imgURL}
-          wrapped
-          ui={false}
-          onLoad={() => setLoaded(true)}
-        />
-        <Card.Content>
-          <Card.Header>
-            {isLoaded ? post?.title : <Placeholder.Line />}
-          </Card.Header>
-          <Card.Meta>
-            <span className="date">
-              {isLoaded ? post.tags?.join(", ") : <Placeholder.Line />}
-            </span>
-          </Card.Meta>
-          <Card.Description>
-            {isLoaded ? (
-              post?.description
-            ) : (
-              <Placeholder.Paragraph>
-                <Placeholder.Line />
-                <Placeholder.Line />
-              </Placeholder.Paragraph>
-            )}
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          {isLoaded ? (
-            <>
-              <LikeComponent />
-              <Button
-                icon="edit"
-                style={{ marginLeft: "20px" }}
-                primary
-                onClick={handleShowModal}
-              />
-              <Button
-                icon="trash"
-                style={{ marginLeft: "20px" }}
-                secondary
-                onClick={() => dispatch(deletePost(post._id))}
-              />
-            </>
-          ) : (
-            <Placeholder.Image />
-          )}
-        </Card.Content>
-      </Card>
-    </> */
-}
