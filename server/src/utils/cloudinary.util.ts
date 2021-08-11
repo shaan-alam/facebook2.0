182051b42ea50c95d7190e636d9eb503f45769e3
Shaan Alam <shaanalam369@gmail.com>
2021-08-11 16:48:46 +0530

    added utility function to format cloudinary urls

--- server/src/utils/cloudinary.util.ts
+++ server/src/utils/cloudinary.util.ts
@@ -9,4 +9,16 @@ cloudinary.v2.config({
   api_secret: `${process.env.CLOUDINARY_API_SECRET}`,
 });
 
+export const formatCloudinaryUrl = (
+  url: string,
+  size: { width: number; height: number },
+  thumb: boolean
+) => {
+  const splitUrl = url.split("upload/");
+  splitUrl[0] += `upload/w_${size.width},h_${size.height}${thumb && ",c_thumb"}/`;
+  const formattedUrl = splitUrl[0] + splitUrl[1];
+
+  return formattedUrl;
+};
+
 export default cloudinary;
