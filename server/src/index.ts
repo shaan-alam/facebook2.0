3629f43f61efb2ad9060584b247fdc41e11302ee
Shaan Alam <shaanalam369@gmail.com>
2021-08-11 16:49:33 +0530

    added file size for uploading

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,8 +14,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 app.use("/static", express.static(path.join(__dirname, "assets")));
 
 const options = {
