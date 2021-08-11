905a2b98b533b5c257d79537dcb7d08c80fd8cf0
Shaan Alam <shaanalam369@gmail.com>
2021-08-11 16:45:28 +0530

    added filter for normal settings

---
+++ client/src/utils/filters.ts
@@ -0,0 +1,9 @@
+export const filters = [
+  { name: "Normal", filter: "" },
+  { name: "Clarendon", filter: "saturate(2)" },
+  { name: "Gingham", filter: "contrast(0.7) saturate(1.5)" },
+  { name: "Moon", filter: "grayscale(1)" },
+  { name: "Lark", filter: "saturate(1.6) hue-rotate(15deg)" },
+  { name: "Reyes", filter: "contrast(0.7)" },
+  { name: "Juno", filter: "hue-rotate(-20deg)" },
+];
