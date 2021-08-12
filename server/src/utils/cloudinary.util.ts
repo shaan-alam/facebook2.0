096ca5011af861f1210fcdd34aa1c57a94b6a6be
Shaan Alam <shaanalam369@gmail.com>
2021-08-12 16:08:33 +0530

    fix: cloudinary format function should return empty string if the url is not defined

--- server/src/utils/cloudinary.util.ts
+++ server/src/utils/cloudinary.util.ts
@@ -14,8 +14,12 @@ export const formatCloudinaryUrl = (
   size: { width: number; height: number },
   thumb: boolean
 ) => {
+  if (!url) return "";
+
   const splitUrl = url.split("upload/");
-  splitUrl[0] += `upload/w_${size.width},h_${size.height}${thumb && ",c_thumb"}/`;
+  splitUrl[0] += `upload/w_${size.width},h_${size.height}${
+    thumb && ",c_thumb"
+  }/`;
   const formattedUrl = splitUrl[0] + splitUrl[1];
 
   return formattedUrl;
