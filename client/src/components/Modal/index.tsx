dc62c016ad2ecf10ecb83a4c4f2cec1b09c51ca0
Shaan Alam <shaanalam369@gmail.com>
2021-08-13 20:12:28 +0530

    Added framer motion animation effects

--- client/src/components/Modal/index.tsx
+++ client/src/components/Modal/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { XIcon } from "@heroicons/react/solid";
+import { motion } from "framer-motion";
 
 const Modal = ({
   isOpen,
@@ -28,12 +29,17 @@ const Modal = ({
 
   return isOpen ? (
     <div className="modal-backdrop px-5 flex justify-center items-center h-screen w-screen fixed top-0 left-0 right-0 bottom-0 bg-black z-10 bg-opacity-80">
-      <div className="w-full sm:w-3/4 md:w-1/2 bg-white rounded-3xl relative p-12">
+      <motion.div
+        className="w-full sm:w-3/4 md:w-1/2 bg-white rounded-3xl relative p-12"
+        initial={{ scale: 0.7, opacity: 1 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ type: "spring", stiffness: 180, duration: 1 }}
+      >
         <a href="#!" onClick={() => setOpen(false)}>
           <XIcon className="text-gray-700 h-10 w-10 absolute right-1 top-1 p-3 bg-gray-300 rounded-full hover:bg-gray-400" />
         </a>
         {children}
-      </div>
+      </motion.div>
     </div>
   ) : null;
 };
