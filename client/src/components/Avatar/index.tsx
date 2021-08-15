e452c2b7b65eaae4ef7ab109c14ae1b22d768198
Shaan Alam <shaanalam369@gmail.com>
2021-08-15 17:14:50 +0530

    added condition rendering with name for avatar component

--- client/src/components/Avatar/index.tsx
+++ client/src/components/Avatar/index.tsx
@@ -1,10 +1,27 @@
 import useUser from "../../hooks/useUser";
 import User from "../../assets/svg/user.svg";
 
-const Avatar = ({ className }: { className: string }) => {
+const Avatar = ({
+  className,
+  name,
+  withName,
+}: {
+  className: string;
+  name?: string;
+  withName?: boolean;
+}) => {
   const user = useUser();
 
-  return (
+  return withName ? (
+    <div className="flex items-center">
+      <img
+        src={user?.avatar ? user?.avatar : User}
+        alt={user?.fullName}
+        className={className}
+      />
+      <p className="ml-2 text-fb font-semibold">{name}</p>
+    </div>
+  ) : (
     <img
       src={user?.avatar ? user?.avatar : User}
       alt={user?.fullName}
