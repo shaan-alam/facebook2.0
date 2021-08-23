c689f7e0a4f02036e216a994b68b9e109ec6fd8e
Shaan Alam <shaanalam369@gmail.com>
2021-08-23 17:58:42 +0530

    completed crud operations for comment replies

--- server/src/controller/commentReply.controller.ts
+++ server/src/controller/commentReply.controller.ts
@@ -31,6 +31,12 @@ export const createCommentReply = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * @function retrieveCommentReplies
+ * @description Used for retrieving comment replies
+ * @param req Express Request Object
+ * @param res Express Response Object
+ */
 export const retrieveCommentReplies = async (req: Request, res: Response) => {
   const { commentId } = req.params;
   const { offset } = req.query;
@@ -41,11 +47,53 @@ export const retrieveCommentReplies = async (req: Request, res: Response) => {
       return res.status(404).json({ message: "No comment found with that ID" });
     }
 
-    const result = await fetchCommentReplies(commentId, offset as string)
+    const result = await fetchCommentReplies(commentId, offset as string);
 
     res.json(result);
   } catch (err) {
     logger.error(err);
     res.status(500).json({ message: err });
+  }
+};
+
+/**
+ * @function editCommentReply
+ * @param req Express Request Object
+ * @param res Express Response Object
+ */
+export const editCommentReply = async (req: Request, res: Response) => {
+  const { commentReplyId } = req.params;
+  const { message } = req.body;
+
+  try {
+    const updatedCommentReply = await CommentReply.findByIdAndUpdate(
+      commentReplyId,
+      { message },
+      {
+        new: true,
+      }
+    );
+
+    res.json(updatedCommentReply);
+  } catch (err) {
+    logger.error(err);
+    res.status(500).json({ message: err });
+  }
+};
+
+/**
+ * @function deleteCommentReply
+ * @param req Express Request Object
+ * @param res Express Response Object
+ */
+export const deleteCommentReply = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    await CommentReply.findByIdAndRemove(id);
+
+    res.status(204).json({ message: "Comment Reply Deleted!!" });
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
