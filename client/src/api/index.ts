69f03e3dc9d582614eacc552278e20d252d0dd93
Shaan Alam <shaanalam369@gmail.com>
2021-08-28 19:47:26 +0530

    comment replies related actions

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -111,3 +111,20 @@ export const editComment = (id: string, message: string) =>
 
 export const fetchCommentReplies = (commentId: string, offset: number) =>
   API.get(`/post/comment-reply/replies/${commentId}?offset=${offset}`);
+
+export const createCommentReply = (commentReply: {
+  message: string;
+  commentId: string;
+  author: string;
+}) => API.post(`/post/comment-reply/reply`, { commentReply });
+
+export const editCommentReply = (commentReply: {
+  commentReplyId: string;
+  message: string;
+}) =>
+  API.patch(`/post/comment-reply/edit-reply/${commentReply.commentReplyId}`, {
+    message: commentReply.message,
+  });
+
+export const deleteCommentReply = (commentReplyId: string) =>
+  API.delete(`/post/comment-reply/delete-reply/${commentReplyId}`);
